c81f4f84e21f17c61b823a451727616ad0d52df2
JGS-JAVA <wlrncom@naver.com>
2025-01-03 17:34:30 +0900

    wish,ranking mapper,xml,controller 추가

--- wishTree/src/main/java/com/topBalance/wishTree/controller/IndexController.java
+++ wishTree/src/main/java/com/topBalance/wishTree/controller/IndexController.java
@@ -3,13 +3,17 @@ package com.topBalance.wishTree.controller;
 import com.topBalance.wishTree.dto.User;
 import com.topBalance.wishTree.service.RankingService;
 import com.topBalance.wishTree.service.WishService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -22,7 +26,12 @@ public class IndexController {
 
     @GetMapping("/") // - 엔드포인트 html 파일에 작성한 화면을 보여줄 주소
     // HTTP GET 요청매핑. 엔드포인트에 대한 요청 처리하고 결과 반환
-    public String ranksindex(Model model) { // model은 index.html에 자바로 작성한 값을 전달할 변수
+    public String ranksindex(Model model,  HttpSession session) { // model은 index.html에 자바로 작성한 값을 전달할 변수
+        Object loggedInUser = addLoggedInUser(session); // 유저컨트롤러에서 로그인시 생성한 세션 불러오기
+        if (loggedInUser != null) {
+            model.addAttribute("loggedInUser", loggedInUser);
+        }
+
         List<Map<String, Object>> ranks = rankingService.getTenRank();
         System.out.println("ranks 목록 확인  : " + ranks);
         model.addAttribute("ranks", ranks);
@@ -35,17 +44,21 @@ public class IndexController {
     }
 
      // DB에 값을 집어넣을 때는 PostMapping 사용하고 엔드포인트 form action에서 작성한 주소를 엔드포인트로 지정
-    @PostMapping("/selectranks-success") // HTTP POST 요청매핑.
-    public String selectSuccess(@ModelAttribute("ranks") User user, Model model) {
+//    @PostMapping("/selectranks-success") // HTTP POST 요청매핑.
+//    public String selectSuccess(@ModelAttribute("ranks") User user, Model model) {
+//
+//        model.addAttribute("msg", "랭킹 조회완료");
+//        return "success";
+//    }
+//    @PostMapping("/selectwishs-success")
+//    public String registerSuccess(@ModelAttribute("wishs") User user, Model model) {
+//
+//        model.addAttribute("msg", "한해소망 조회완료");
+//        return "success"; //회원가입이 무사히 완료될 경우 success 페이지로 이동
+//    }
 
-        model.addAttribute("msg", "랭킹 조회완료");
-        return "success";
+    @ModelAttribute("loggedInUser")
+    public Object addLoggedInUser(HttpSession session) {
+        return session.getAttribute("loggedInUser");
     }
-    @PostMapping("/selectwishs-success")
-    public String registerSuccess(@ModelAttribute("wishs") User user, Model model) {
-
-        model.addAttribute("msg", "한해소망 조회완료");
-        return "success"; //회원가입이 무사히 완료될 경우 success 페이지로 이동
-    }
-
 }
